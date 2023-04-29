16b84bc251222c497678656d21a4a72ff16f1010
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-04-29 12:58:57 +0100

    Pause Menu (Needs player controller to test)

---
+++ Assets/oD Wreckz/Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public PlayerController playerController;
+    public GameObject pauseMenuUI;
+
+    //Get the player script
+    private void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //So that the player cant pause after death
+        if (/*player is not dead*/playerController)
+        {
+            //If start button or escape pressed
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        //Take away UI and set time back to normal
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        //Add UI and set time to stopped
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        //When you return to menu reset paused time
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        Resume();
+    }
+
+    public void Quit()
+    {
+        //Quit
+        Application.Quit();
+    }
+}
