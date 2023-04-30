cf8448c5086176cd93b3b440def285f3b6f767a3
IbrahimOezhan <ibrahimoezhan1207@gmail.com>
2023-04-30 14:00:08 +0200

    Enemy Models + Animations

--- Assets/Ibrahim/Scripts/Enemy/Enemy2.cs
+++ Assets/Ibrahim/Scripts/Enemy/Enemy2.cs
@@ -4,16 +4,16 @@ using UnityEngine.AI;
 public class Enemy2 : MonoBehaviour, IPokeble
 {
     int health = 1;
-    float timer;
+    Animator anim;
     Player_Movement cart;
     NavMeshAgent agent;
-    [HideInInspector] public bool hasTheGoods;
     [SerializeField] public float speed;
     [SerializeField] private Rigidbody ninjaStar;
     [SerializeField] private Transform spawn;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         cart = FindObjectOfType<Player_Movement>();
         agent.speed = agent.speed + Random.Range(-.5f, .5f);
@@ -21,14 +21,28 @@ public class Enemy2 : MonoBehaviour, IPokeble
 
     void Update()
     {
-        timer -= Time.deltaTime;
-        if (timer < 0)
+
+        AnimatorClipInfo[] clipInfo = anim.GetCurrentAnimatorClipInfo(0);
+        string currentStateName = "test";
+        if (clipInfo.Length > 0)
         {
-            Vector3 pos = new Vector3(cart.transform.position.x, 0, cart.transform.position.z);
-            Instantiate(ninjaStar, spawn.position, Quaternion.identity).AddForce((pos - transform.position).normalized * speed, ForceMode.Force);
-            timer = 2;
+            currentStateName = clipInfo[0].clip.name;
+            Debug.Log("Current Animation State: " + currentStateName);
         }
+        if (agent.velocity != Vector3.zero) anim.Play("Run");
+        else
+        {
+            transform.LookAt(cart.transform);
+            if (currentStateName != "Idle" && currentStateName != "Throw") anim.Play("Idle");
+        }
+
         agent.SetDestination(cart.transform.position);
+    }
+
+    public void Throw()
+    {
+        Vector3 pos = new Vector3(cart.transform.position.x, 0, cart.transform.position.z);
+        Instantiate(ninjaStar, spawn.position, Quaternion.identity).AddForce((pos - transform.position).normalized * speed, ForceMode.Force);
     }
 
     public void OnPoke(Vector3 impulse)
